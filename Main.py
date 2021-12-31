4baf2c6ef0f6a81c0615eb4d85cc3306323d9b0a
Ioanna Moraiti <t8190121@aueb.gr>
2021-12-31 19:00:04 +0200

    added Route class, changed distances to matrix, added Solver code NOT DONE

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 from Model import Model
+from Solver import *
 
 model = Model()
 model.build_model()
+s = Solver(model)
+sol = s.solve()
--- Model.py
+++ Model.py
@@ -11,34 +11,44 @@ class Node:
         self.demand = int(demand)
         self.service_time = int(service_time)
         self.profit = int(profit)
-        self.visited = False
+        self.isRouted = False
 
 
 class Model:
     # Instance variables
     def __init__(self):
-        self.all_nodes = []  # List of all model nodes
+        self.allNodes = []  # List of all model nodes
         self.customers = []  # List of all customers
         self.max_capacity = -1  # Max capacity
         self.max_duration = -1  # Max duration
         self.vehicles = -1  # Available vehicles
-        self.distances = dict()  # Dict containing all distances from one node to another
 
     def build_model(self):
         self.max_capacity = csv_reader.get_capacity()
         self.max_duration = csv_reader.get_duration()
         self.vehicles = csv_reader.get_vehicles()
         depot = Node(0, csv_reader.get_dx(), csv_reader.get_dy(), 0, 0, 0)
-        self.all_nodes.append(depot)
+        self.allNodes.append(depot)
         cust_data_lists = csv_reader.get_customer_data()
-        custs_map = map(lambda c: Node(c['id'], c['x'], c['y'], c['demand'], c['service_time'], c['profit']), cust_data_lists)
+        custs_map = map(lambda c: Node(int(c['id']), c['x'], c['y'], c['demand'], c['service_time'], c['profit']), cust_data_lists)
         self.customers.extend(list(custs_map))
-        self.all_nodes.extend(self.customers)
+        self.allNodes.extend(self.customers)
+        self.distances = [[0.0 for x in range(len(self.allNodes))] for y in range(len(self.allNodes))]  # Dict containing all distances from one node to another
 
         # Dict keys are all possible combinations of 2 nodes, without repetition
-        for key in itertools.combinations(self.customers, 2):
-            a = key[0]
-            b = key[1]
-            # Dict values are the distances
-            dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
-            self.distances[key] = dist
+        for i in range(0, len(self.allNodes)):
+            for j in range(0, len(self.allNodes)):
+                a = self.allNodes[i]
+                b = self.allNodes[j]
+                dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
+                self.distances[i][j] = dist
+
+class Route:
+    def __init__(self, dp, cap, dur):
+        self.sequenceOfNodes = []
+        self.sequenceOfNodes.append(dp)
+        self.sequenceOfNodes.append(dp)
+        self.cost = 0
+        self.capacity = cap
+        self.duration = dur
+        self.load = 0
